daae7de1fb66c0fac553b093ba2c72cc06b8eecd
zaRT0 <andrey.panyavkin@mail.ru>
2025-12-06 22:26:46 +0400

    add_programcs

--- Airline/Airline.AppHost/Program.cs
+++ Airline/Airline.AppHost/Program.cs
@@ -1,14 +1,30 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
+var kafkaTopic = builder.AddParameter("KafkaTopic", "ticket-events");
+var producerIntervalMs = builder.AddParameter("KafkaProducerIntervalMs", "3000");
+var producerBatchSize = builder.AddParameter("KafkaProducerBatchSize", "2");
+var consumerGroup = builder.AddParameter("KafkaConsumerGroup", "airline-api-consumer");
+
 var mssql = builder.AddMySql("mysql");
 var mssqlDb = mssql.AddDatabase("AirlineDb");
 
+var kafka = builder.AddKafka("Kafka")
+       .WithKafkaUI();
+
 builder.AddProject<Projects.Airline_Api>("AirlineAppAPI")
-    .WithReference(mssqlDb, "DefaultConnection")
-    .WaitFor(mssqlDb);
+       .WithReference(mssqlDb, "DefaultConnection")
+       .WithReference(kafka, "KafkaConnection")
+       .WithEnvironment("KafkaTopic", kafkaTopic)
+       .WithEnvironment("Kafka_ConsumerGroup", consumerGroup)
+       .WaitFor(mssqlDb)
+       .WaitFor(kafka);
 
-builder.AddProject<Projects.Airline_Generator>("airline-generator");
 
-builder.AddProject<Projects.Airline_Gen>("airline-gen");
+builder.AddProject<Projects.Airline_Generator>("airline-generator")
+       .WithReference(kafka, "KafkaConnection")
+       .WithEnvironment("KafkaTopic", kafkaTopic)
+       .WithEnvironment("KafkaProducerIntervalMs", producerIntervalMs)
+       .WithEnvironment("KafkaProducerBatchSize", producerBatchSize)
+       .WaitFor(kafka);
 
 builder.Build().Run();
